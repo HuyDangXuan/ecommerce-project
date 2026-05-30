3ac4e1921e224a1fcd7694a5b5b9ccd527ccbb44
HuyDangXuan <hhunu114@gmail.com>
2026-05-30 08:24:52 +0700

    Add mini cart, mobile menu, and progress wrap components
    - Created a new mini cart component with product details and subtotal.
    - Implemented a mobile menu area with navigation links and dropdowns for categories.
    - Added a progress wrap component with a circular progress indicator.

--- index.ts
+++ index.ts
@@ -1,12 +1,26 @@
-import express from 'express'
+import express, {Request, Response} from 'express'
+import path from 'path'
 
 const app = express()
 const port = 3000
+const viewsPath = path.join(__dirname, 'views')
+const publicPath = path.join(__dirname, 'public')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.set('views', viewsPath)
+app.set('view engine', 'pug')
+
+app.use(express.static(publicPath))
+
+app.get('/', (req: Request, res: Response) => {
+  res.render('client/pages/home')
+})
+
+app.get('/admin/dashboard', (req: Request, res: Response) => {
+  res.render('admin/pages/dashboard', {
+    title: 'Trang tổng quan',
+  })
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Website đang chạy trên cổng ${port}`)
 })
